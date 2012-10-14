ecc8e13fe1a71b786f6a8f42746be4d50a8a4cc6
Ben James <ben.james@guardian.co.uk>
2012-10-14 18:28:02 +0100

    Use ByteChunk type for request/response

--- src/main/scala/com/gu/grauniator/aws/SNS.scala
+++ src/main/scala/com/gu/grauniator/aws/SNS.scala
@@ -2,7 +2,9 @@ package com.gu.grauniator
 package aws
 
 import akka.dispatch.Future
+import blueeyes.core.data.ByteChunk
 import blueeyes.core.http.HttpResponse
+import blueeyes.core.http.HttpHeaders.`Content-Type`
 import blueeyes.core.http.MimeTypes.{application, `x-www-form-urlencoded`}
 import blueeyes.core.service.engines.HttpClientXLightWeb
 import scalaz._, Scalaz._
@@ -13,6 +15,7 @@ import org.joda.time.format.ISODateTimeFormat
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 import org.apache.commons.codec.binary.Base64
+import blueeyes.core.service.HttpClient
 
 
 /*
@@ -23,13 +26,12 @@ final class SNS(awsId: String, awsKey: String, endpoint: String) {
   import SNS._
   import blueeyes.core.data.BijectionsChunkString._
 
-  val client =
+  val client: HttpClient[ByteChunk] =
     new HttpClientXLightWeb()
       .path("http://" + endpoint)
-      .contentType(application/`x-www-form-urlencoded`)
-      .translate[String]
+      .header(`Content-Type`(application/`x-www-form-urlencoded`))
 
-  def publish(subject: String, topicArn: String, message: String): Future[HttpResponse[String]] = {
+  def publish(subject: String, topicArn: String, message: String): Future[HttpResponse[ByteChunk]] = {
     val params = List(
       "Subject" -> subject,
       "TopicArn" -> topicArn,
@@ -42,7 +44,7 @@ final class SNS(awsId: String, awsKey: String, endpoint: String) {
     )
     val signature = signRequest("POST", "/", params)
     val signedParams = ("Signature" -> signature) :: params
-    client.post[String]("/")(canonicalQueryString(signedParams))
+    client.post("/")(canonicalQueryString(signedParams))
   }
 
   private def signRequest(method: String, path: String, params: List[(String, String)]): String = {
@@ -69,6 +71,6 @@ object SNS {
 
   val algorithm = "HmacSHA256"
 
-  def urlEncode(s: String): String = java.net.URLEncoder.encode(s, "utf-8").replaceAllLiterally("+", "%20")
+  def urlEncode(s: String): String = java.net.URLEncoder.encode(s, charset).replaceAllLiterally("+", "%20")
   
 }
